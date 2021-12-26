82212ac3f00155cea5d7578fd2a8da63ba6bcb54
Hilman Taris Muttaqin <hilmanmuttaqin345@gmail.com>
2021-12-27 00:30:41 +0700

    Add address for event participant

---
+++ event_participant.h
@@ -0,0 +1,17 @@
+//
+// Created by Hilman Taris M on 12/26/2021.
+//
+
+#ifndef LINKED_LIST_EVENT_PARTICIPANT_H
+#define LINKED_LIST_EVENT_PARTICIPANT_H
+
+#include "participant.h"
+
+typedef struct elmEventParticipant *adr_event_participant;
+
+struct elmEventParticipant {
+    adr_event_participant next, prev;
+    adr_participant info;
+};
+
+#endif //LINKED_LIST_EVENT_PARTICIPANT_H
